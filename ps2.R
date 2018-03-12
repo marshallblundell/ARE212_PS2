440ca874fd37ab5d011fa7a8be37c9f5f177b5b7
Marshall Blundell <mm.blundell@gmail.com>
2018-03-11 19:43:00 -0700

    Set up and loaded data.

--- ps2.R
+++ ps2.R
@@ -2,8 +2,23 @@
 #     File Name           :     ps2.R
 #     Created By          :     MBlundell
 #     Creation Date       :     [2018-03-11 19:25]
-#     Last Modified       :     [AUTO_UPDATE_BEFORE_SAVE]
+#     Last Modified       :     [2018-03-11 19:42]
 #     Description         :      
 #############################################################
 
+# 
+# Set up.
+#
+library(pacman)
+p_load(readr, xlsx)
+
+dir <- "C:/Users/mblundell/Documents/ARE/ARE212/PS2/"
+originaldata <- paste0(dir, "OriginalData/")
+output <- paste0(dir, "Output/")
+
+#
+# Load data and clean it.
+#
+data <- read.xlsx2(paste0(originaldata, "nerlove.xls"),
+                   sheetIndex = 1)
 
